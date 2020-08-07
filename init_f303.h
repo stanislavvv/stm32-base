ca720d01efa3a4670ab093f41b75f216971ac8ba
Stanislav Vlasov <stanislav.v.v@gmail.com>
2020-08-08 01:46:48 +0500

    real clock init for stm32f303

--- init_f303.h
+++ init_f303.h
@@ -2,6 +2,7 @@
 #include <libopencm3/stm32/gpio.h>
 
 static inline void init_gpio(void) {
+    rcc_clock_setup_pll(&rcc_hse8mhz_configs[RCC_CLOCK_HSE8_72MHZ]);
     rcc_periph_clock_enable(RCC_GPIOC);
     gpio_mode_setup(
         GPIOC, GPIO_MODE_OUTPUT,
