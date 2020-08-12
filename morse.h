6c9e4b31ea80d4462ba9bda978b4a5f7a924b517
Stanislav Vlasov <stanislav.v.v@gmail.com>
2020-08-12 10:32:33 +0500

    slightly change morse.h

--- morse.h
+++ morse.h
@@ -3,6 +3,9 @@
 
 #define DIT_LENGTH 200 /* length of dot in ms */
 #define DAH_LENGTH DIT_LENGTH * 3 /* length of dash in ms */
+#define LED_PORT GPIOC
+#define LED_PIN GPIO13
+
 
 /* long pause */
 #define lspc() vTaskDelay(pdMS_TO_TICKS(DAH_LENGTH));
@@ -10,9 +13,9 @@
 /* short pause */
 #define sspc() vTaskDelay(pdMS_TO_TICKS(DIT_LENGTH));
 
-#define LED_on() gpio_clear(GPIOC,GPIO13);
+#define LED_on() gpio_clear(LED_PORT,LED_PIN);
 
-#define LED_off() gpio_set(GPIOC,GPIO13);
+#define LED_off() gpio_set(LED_PORT,LED_PIN);
 
 /* send morse character */
 void send_letter(char c);
